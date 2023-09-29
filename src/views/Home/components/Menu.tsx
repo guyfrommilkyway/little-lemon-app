155552a42c00a02edb265be853a9c1f5c45d8920
Almer Tampus <almerflorestampus@gmail.com>
2023-09-29 16:35:38 +0800

    Add Menu component for Home

---
+++ src/views/Home/components/Menu.tsx
@@ -0,0 +1,21 @@
+// packages
+import React from 'react';
+
+// types
+import { MenuProps } from '@/types';
+
+const Menu: React.FC<MenuProps> = props => {
+  const { render } = props;
+
+  return (
+    <div className='w-full my-10 lg:my-[280px]'>
+      <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center mb-8'>
+        <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-0'>This weeks specials!</h1>
+        <button className='px-4 py-2 font-bold bg-primary-2 rounded-[16px]'>Online Menu</button>
+      </div>
+      <div className='flex flex-col items-stretch md:flex-row gap-8 w-full'>{render()}</div>
+    </div>
+  );
+};
+
+export default Menu;
