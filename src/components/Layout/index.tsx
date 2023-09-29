5eaa3bbe33bffe925df481ba9e9c8c3bb8278935
Almer Tampus <almerflorestampus@gmail.com>
2023-09-29 16:34:04 +0800

    Add Layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,27 @@
+// packages below
+import React, { Fragment } from 'react';
+
+// components below
+import Head from '@/components/Head';
+import Topbar from '@/components/Topbar';
+import Footer from '@/components/Footer';
+
+// types below
+import { LayoutProps } from '@/types';
+
+const Layout: React.FC<LayoutProps> = props => {
+  const { title, children } = props;
+
+  return (
+    <Fragment>
+      <Head title={title} />
+      <main id='layout' className='flex flex-col w-full h-full min-h-screen bg-white'>
+        <Topbar />
+        <section>{children}</section>
+        <Footer />
+      </main>
+    </Fragment>
+  );
+};
+
+export default Layout;
