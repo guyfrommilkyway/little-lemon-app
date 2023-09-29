62af717cf614a91c6b99e73797e59c0df477e8ae
Almer Tampus <almerflorestampus@gmail.com>
2023-09-30 05:44:39 +0800

    Integrate react-toastify

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+import { ToastContainer } from 'react-toastify';
 
 // components below
 import App from './App.tsx';
 
 // assets below
 import './index.css';
+import '../node_modules/react-toastify/dist/ReactToastify.min.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
+      <ToastContainer />
     </HelmetProvider>
   </React.StrictMode>,
 );
