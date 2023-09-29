8a5f32e583c17f53061245ffe85d1b917ce53375
Almer Tampus <almerflorestampus@gmail.com>
2023-09-30 05:45:19 +0800

    Update Booking component

--- src/views/Booking/index.tsx
+++ src/views/Booking/index.tsx
@@ -1,12 +1,18 @@
-// packages below
+// packages
 import React from 'react';
 
+// components
+import Layout from '@/components/Layout';
+import Form from './components/Form';
+import Header from './components/Header';
+
 const Booking: React.FC = () => {
-	return (
-		<div>
-			<span>Booking</span>
-		</div>
-	);
+  return (
+    <Layout title='Little Lemon'>
+      <Header />
+      <Form />
+    </Layout>
+  );
 };
 
 export default Booking;
